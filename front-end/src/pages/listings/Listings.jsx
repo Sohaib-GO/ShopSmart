9e00c227c4d44809bd07cacbfefb11e6ba98b45b
Margaryta <margaryta.lanova@gmail.com>
2023-02-05 14:46:59 -0800

    initial commit

--- front-end/src/pages/listings/Listings.jsx
+++ front-end/src/pages/listings/Listings.jsx
@@ -14,14 +14,22 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import FolderIcon from "@mui/icons-material/Folder";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import storeLogo from "../../images/safeway-Logo.png";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 const mockData = [
-  { id: "1", name: "apple", price: "5.00" },
+  {
+    id: "1",
+    name: "apple",
+    price: "5.00",
+    img: "https://i5.walmartimages.ca/images/Enlarge/094/514/6000200094514.jpg",
+  },
   { id: "2", name: "banana", price: "3.00" },
   { id: "3", name: "caviar", price: "52.00" },
   { id: "4", name: "bread", price: "4.25" },
@@ -34,11 +42,11 @@ const mockData = [
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 function Listings() {
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
+  const [dense, setDense] = useState(false);
+  const [item, setItem] = useState(null);
   return (
-    <>
-      <div>
+    <div className="listings-page">
+      <div className="store-table">
         <div className="search-field">
           <TextField
             id="filled-basic"
@@ -54,9 +62,11 @@ function Listings() {
         </div>
         <div>
           <Grid item xs={12} md={6}>
-            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-              Avatar with text and icon
-            </Typography>
+            <Typography
+              sx={{ mt: 4, mb: 2 }}
+              variant="h6"
+              component="div"
+            ></Typography>
             <List dense={dense}>
               {mockData.map((mock) => {
                 return (
@@ -69,10 +79,13 @@ function Listings() {
                       </IconButton>
                     }
                   >
-                    <ListItemAvatar>
+                    <ListItemAvatar
+                      className="store-logo-avatar"
+                      onClick={() => setItem(mock)}
+                    >
                       <Avatar src={storeLogo} />
                     </ListItemAvatar>
-                    <ListItemText primary={mock.name} />
+                    <ListItemText className="item-name" primary={mock.name} />
                     <ListItemText primary={`${mock.price}$`} />
                   </ListItem>
                 );
@@ -81,8 +94,38 @@ function Listings() {
           </Grid>
         </div>
       </div>
-      <div className="drawer"></div>
-    </>
+      {item && (
+        <div className="drawer">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {mockData.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  DescriptionDescriptionDescriptionDescriptionDescription
+                  DescriptionDescriptionDescriptionDescriptionDescription
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        // <div className="drawer">
+        //   <img className="item-image" src={item.img}></img>
+        // </div>
+      )}
+    </div>
   );
 }
 
