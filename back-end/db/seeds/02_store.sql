585d0cb50ba9f2d96ad5a6e907c031fbcb40cec0
Sohaib-GO <s.almajmaie@gmail.com>
2023-02-05 16:47:59 -0700

    Add Seed files

---
+++ back-end/db/seeds/02_store.sql
@@ -0,0 +1,10 @@
+INSERT INTO stores (name, address)
+VALUES
+('Walmart', '123 Main Street'),
+('Target', '456 Elm Street'),
+('Costco', '789 Oak Boulevard'),
+('Kroger', '111 Market Ave'),
+('Safeway', '222 High Road');
+
+
+
