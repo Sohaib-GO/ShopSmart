65cc815541b7a86ad5862adeb752a2ca63362e6f
Margaryta <margaryta.lanova@gmail.com>
2023-02-04 17:05:06 -0800

    added functionality to buttons

--- front-end/src/pages/homepage/Homepage.jsx
+++ front-end/src/pages/homepage/Homepage.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+import "./Homepage.css";
+
 function Homepage() {
+  const navigate = useNavigate();
+
   return (
     <div className="Homepage">
-      <h2>Homepage</h2>
+      <Typography className="text" variant="h6" gutterBottom>
+        Discover,Compare & Choose
+      </Typography>
+      <div className="get_started_button">
+        <Button variant="contained" onClick={() => navigate("/listings")}>
+          <Typography variant="h6" gutterBottom>
+            Start Searching
+          </Typography>
+        </Button>
+      </div>
     </div>
   );
 }
