e1811ffc319b33247d7c620592c695595fc0740f
Margaryta <margaryta.lanova@gmail.com>
2023-02-06 11:21:39 -0800

    added functionality to saved items drawer, implement the function addItem

--- front-end/src/pages/listings/Listings.jsx
+++ front-end/src/pages/listings/Listings.jsx
@@ -41,9 +41,13 @@ const mockData = [
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
-function Listings() {
+function Listings({ setSavedItems }) {
   const [dense, setDense] = useState(false);
   const [item, setItem] = useState(null);
+
+  function addItems(item) {
+    setSavedItems((prev) => [...prev, item]);
+  }
   return (
     <div className="listings-page">
       <div className="store-table">
@@ -97,26 +101,29 @@ function Listings() {
       {item && (
         <div className="drawer">
           <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {mockData.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  DescriptionDescriptionDescriptionDescriptionDescription
-                  DescriptionDescriptionDescriptionDescriptionDescription
-                </Typography>
-              </CardContent>
-            </CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={item.img}
+              alt={item.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                DescriptionDescriptionDescriptionDescriptionDescription
+                DescriptionDescriptionDescriptionDescriptionDescription
+              </Typography>
+            </CardContent>
+
             <CardActions>
-              <Button size="small" color="primary">
-                Share
+              <Button
+                onClick={() => addItems(item)}
+                size="small"
+                color="success"
+              >
+                Save
               </Button>
             </CardActions>
           </Card>
