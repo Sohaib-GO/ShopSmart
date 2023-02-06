fbf2429d7b4c4514ca2e1514c5f31d30aa0f1953
Sohaib-GO <s.almajmaie@gmail.com>
2023-02-06 14:17:22 -0700

    Fix Issue with Login
    Fix issue where the user has to login again upon refresh

--- back-end/server.js
+++ back-end/server.js
@@ -32,27 +32,13 @@ app.post("/api/users", (req, res) => {
   }
 
   db.query(
-    `SELECT email FROM users WHERE email = $1`,
-    [email],
-    (error, result) => {
+    `INSERT INTO users (name, email, password, address) values ($1, $2, $3, $4)`,
+    [name, email, password, address],
+    (error) => {
       if (error) {
         return res.status(500).json({ error });
       }
-
-      if (result.rows.length > 0) {
-        return res.status(400).json({ error: "email already exists" });
-      }
-
-      db.query(
-        `INSERT INTO users (name, email, password, address) values ($1, $2, $3, $4)`,
-        [name, email, password, address],
-        (error) => {
-          if (error) {
-            return res.status(500).json({ error });
-          }
-          res.status(201).json({ success: true });
-        }
-      );
+      res.status(201).json({ success: true });
     }
   );
 });
@@ -61,7 +47,7 @@ app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: "email and password are required" });
+    return res.status(400).json({ error: "Email and password are required." });
   }
 
   db.query(
@@ -70,13 +56,12 @@ app.post("/api/login", (req, res) => {
     (error, result) => {
       if (error) {
         return res.status(500).json({ error });
-
       }
       if (result.rows.length === 0) {
-        return res.status(401).json({ error: "invalid email or password" });
+        return res.status(401).json({ error: "Invalid email or password." });
       }
       const user = result.rows[0];
-      res.cookie("user", user.id);
+      res.cookie("user", user.id, { maxAge: 60 * 60 * 1000 });
       res.json(user);
     }
   );
@@ -84,7 +69,7 @@ app.post("/api/login", (req, res) => {
 
 app.post("/api/logout", (req, res) => {
   res.clearCookie("user");
-  res.json('Logged out successfully');
+  res.json({ message: "Logged out successfully." });
 });
 
 // create a new list
--- front-end/src/authentication/login.jsx
+++ front-end/src/authentication/login.jsx
@@ -1,69 +1,118 @@
 import React, { useState, useEffect } from "react";
 
 function Login() {
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState("");
-const [success, setSuccess] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-useEffect(() => {
-if (success) {
-setTimeout(() => {
-window.location.href = "/home";
-}, 3000);
-}
-}, [success]);
-
-const handleSubmit = (e) => {
-e.preventDefault();
-
-const user = { email, password };
-
-fetch("/api/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(user),
-})
-  .then((res) => {
-    if (res.status === 200) {
+  useEffect(() => {
+    // check if user is already logged in
+    const user = localStorage.getItem("user");
+    if (user) {
       setSuccess(true);
-      setError("");
-    } else {
-      return res.json();
+      setIsLoggedIn(true);
     }
-  })
-  .then((data) => {
-    if (data && data.error) {
-      setError(data.error);
+  }, []);
+
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        setSuccess(false);
+      }, 3000);
     }
-  });
-};
+  }, [success]);
 
-return (
-<>
-{success && <div>Login Successful</div>}
-{error && <div>{error}</div>}
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-<form onSubmit={handleSubmit}>
-    <input
-      type="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      placeholder="Email"
-      required
-    />
-    <input
-      type="password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      placeholder="Password"
-      required
-    />
-    <button type="submit">Login</button>
-  </form>
-</>
-);
+    const user = { email, password };
+
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setSuccess(true);
+          setIsLoggedIn(true);
+          setError("");
+          localStorage.setItem("user", JSON.stringify(data));
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setIsLoggedIn(false);
+
+    fetch("/api/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setSuccess(false);
+          setError("");
+        }
+      });
+  };
+
+  return (
+    <>
+      {success && <div>Login Successful</div>}
+      {error && <div>{error}</div>}
+
+      {isLoggedIn && (
+        <div>
+          <br />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+
+      {!isLoggedIn && (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </>
+  );
 }
+
 export default Login;
