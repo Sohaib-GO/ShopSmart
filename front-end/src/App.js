078dd9095fce369aa4e5e3723605b874d7d6e8fb
Sohaib-GO <s.almajmaie@gmail.com>
2023-02-06 11:11:11 -0700

    Add search functionality
    The user could now search for an item
    and see relevant info

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import NewListForm from './components/NewList/NewList';
 import Listings from './pages/listings/Listings';
-
+import Items from './components/items/items';
 function App() {
 
 
 
   return (
     <div className="App">
-<NewListForm />
-    <Listings />
+    <Items />
+    
     </div>
   );
 }
