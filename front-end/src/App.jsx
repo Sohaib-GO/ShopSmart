886736ee4e854c76196a49405e37ffc1b9c25eb4
Margaryta <margaryta.lanova@gmail.com>
2023-02-06 11:22:53 -0800

    created new route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -6,13 +6,24 @@ import Homepage from "./pages/homepage/Homepage";
 import Listings from "./pages/listings/Listings";
 
 function App() {
+  const [savedItems, setSavedItems] = useState([]);
+
+  console.log(savedItems);
+
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/listings" element={<Listings />} />
+          <Route
+            path="/listings"
+            element={<Listings setSavedItems={setSavedItems} />}
+          />
+          <Route
+            path="/saved-items"
+            // element={<SavedItems savedItems={savedItems} />}
+          />
         </Routes>
       </div>
     </Router>
