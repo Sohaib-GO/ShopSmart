e036a11331b7d4fc7cf0f7a66b0b48095e107506
Njoe00 <nick.james.joe@gmail.com>
2023-02-09 23:39:22 +0000

    adding mapping markers and info box

--- front-end/src/pages/map/Maps.jsx
+++ front-end/src/pages/map/Maps.jsx
@@ -3,10 +3,14 @@ import {
   useLoadScript,
   Marker,
   useJsApiLoader,
+  InfoWindow,
 } from "@react-google-maps/api";
 import { useMemo } from "react";
+import {useState} from "react";
 import React from "react";
 // import Map from "../../components/Map/Map";
+
+const [selectedMarker, setSelectMarker] = useState("")
 
 const containerStyle = {
     width: '1980px',
@@ -14,23 +18,38 @@ const containerStyle = {
   };
   
   const center = {
-    lat: 49.2827,
-    lng: -123.1207
+    lat: 49.263760,
+    lng: -123.148330
   };
 
+  const location= {
+    lat: 49.263760,
+    lng: -123.148330
+  }
+  
+  const divStyle ={
+    background: `white`,
+    border: `1 px solid #ccc`,
+    padding: 15
+  }
+
+  
 export default function Maps_test() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyDxSBp4edh5BzrKcIJa6ZrP7G5tQJVNFKo",
   });
 
+  const onClick = 
+
   const [map, setMap] = React.useState(null);
+
 
   const onLoad = React.useCallback(function callback(map) {
 
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-
+    
     setMap(map);
   }, []);
 
@@ -41,14 +60,25 @@ export default function Maps_test() {
   if (!isLoaded) return <div>Loading...</div>;
   return (
     <GoogleMap
+      id='Marker-test'
       mapContainerStyle={containerStyle}
       center={center}
       zoom={10}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
+    <InfoWindow 
+      position={location}
+    >
+      <div style={divStyle}>
+        <h1>Info</h1>
+      </div>
+    </InfoWindow>
+    <Marker 
+      position={location}
+      onClick={()=>{setSelectMarker(element)}}
+    />
+        <></>
     </GoogleMap>
   );
 }
