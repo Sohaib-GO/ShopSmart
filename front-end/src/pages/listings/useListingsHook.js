2dcc85191d4e2ecb2379962928a5dd75175a6923
Sohaib-GO <s.almajmaie@gmail.com>
2023-02-09 17:56:32 -0700

    Create Hook for Groceries

---
+++ front-end/src/pages/listings/useListingsHook.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useGroceryList = (props) => {
+  const [groceries, setGroceries] = useState([]);
+
+
+  useEffect(() => {
+    fetch("/api/fetch-grocery-list")
+      .then((res) => res.json())
+      .then((data) => setGroceries(data.data));
+  }, []);
+
+  const handleDeleteGroceryItem = async (item_name, store_name) => {
+    try {
+      const response = await axios.post("/api/delete-grocery-item", { item_name, store_name });
+      if (response.data.success) {
+        setGroceries(prevGroceryList =>
+          prevGroceryList.filter(store => {
+            store.items = store.items.filter(item => item.item_name !== item_name);
+            return store.items.length > 0;
+          })
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return { groceries, handleDeleteGroceryItem };
+};
